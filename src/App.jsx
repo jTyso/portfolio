e940f7d9e7e7c9c24a53274f2769e84e4fd2a5dd
Jordan Theisen <jordantheisen08@gmail.com>
2023-10-15 00:09:14 -0500

    first deployment, god speed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import './css/App.css';
-import NavBar from './components/NavBar'
-import AboutMe from './components/AboutMe'
+import "./css/App.css";
+import NavBar from "./components/NavBar";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
     <div>
-      <NavBar/>
-        <main>
-          <AboutMe/>
-        </main>
+      <nav class="navbar">
+        <div class="logo">JT</div>
+        <ul class="nav-links">
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="main-container">
+        <h3>Business and Technology Enchanter</h3>
+        <h1>JORDAN THEISEN</h1>
+      </div>
     </div>
   );
 }
